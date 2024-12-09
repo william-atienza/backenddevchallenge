c1be1df6062fecc39af9f227250c367303fcf79a
william-atienza <william.atienza@yahoo.com>
2024-12-10 01:37:01 +0800

    Separate service interface and implementation

--- src/main/java/com/limepay/backenddevchallenge/controller/MainController.java
+++ src/main/java/com/limepay/backenddevchallenge/controller/MainController.java
@@ -1,16 +1,14 @@
 package com.limepay.backenddevchallenge.controller;
 
 import com.limepay.backenddevchallenge.service.DirectorService;
+import com.limepay.backenddevchallenge.service.DirectorServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
 
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
