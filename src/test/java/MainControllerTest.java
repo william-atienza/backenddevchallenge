d3ccaf0296b467636ecca2ac7ec43eda7835249b
william-atienza <william.atienza@yahoo.com>
2024-12-10 06:10:24 +0800

    Add controller and service unit test

--- src/test/java/MainControllerTest.java
+++ src/test/java/MainControllerTest.java
@@ -1,30 +1,65 @@
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.limepay.backenddevchallenge.controller.MainController;
+import com.limepay.backenddevchallenge.service.DirectorService;
+import com.limepay.backenddevchallenge.service.DirectorServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.ArrayList;
+import java.util.List;
 
-@SpringBootTest
+@WebMvcTest
 public class MainControllerTest {
-    @Autowired
+
+    MockMvc mockMvc;
+
+    @Mock
+    DirectorService service;
+
+    @InjectMocks
     MainController controller;
 
-    @Test
-    void contextLoads() throws Exception{
-        assertThat(controller).isNull();
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
-    void getDirectorsTest(){
-//        WebTestClient client = MockMvc .bindToController(new MainController()).build();
-//        client.get().uri("/api/directors?threshold=1")
-//                .accept(MediaType.APPLICATION_JSON)
-//                .exchange()
-//                .expectStatus().isOk()
-//                .expectHeader().contentType(MediaType.APPLICATION_JSON);
+    void testIndex() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string("Hi Limepay!"));
     }
+
+
+   @Test
+   void testGetDirectors() throws Exception {
+        List<String> directors = new ArrayList<>();
+        directors.add("Quentin Tarantino");
+        when(service.getDirectors(2)).thenReturn(directors);
+        this.mockMvc.perform(get("/api/directors?threshold=2")).andDo(print()).andExpect(status().isOk())
+         .andExpect(content().string(containsString("Quentin Tarantino")));
+   }
+
+   @Configuration
+   @Import(DirectorServiceImpl.class)
+   static class Config{
+
+   }
 }
--- src/test/java/com/limepay/backenddevchallenge/DirectorServiceTest.java
+++ src/test/java/com/limepay/backenddevchallenge/DirectorServiceTest.java
@@ -1,8 +1,61 @@
 package com.limepay.backenddevchallenge;
 
+import com.limepay.backenddevchallenge.dto.Movie;
+import com.limepay.backenddevchallenge.dto.MovieRecord;
+import com.limepay.backenddevchallenge.service.DirectorService;
+import com.limepay.backenddevchallenge.service.DirectorServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestClient;
 
-@ExtendWith(MockitoExtension.class)
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
+@SpringBootTest(classes = DirectorService.class)
+@ActiveProfiles("test")
 public class DirectorServiceTest {
+    @Value("${limepay.movies.url}")
+    private String MOVIES_URL = "https://wiremock.dev.eroninternational.com/api/movies/search?page=1";
+
+    @Mock
+    private DirectorService service;
+
+
+    @Test
+    void getDirectorsTest(){
+        List<String> actors = Arrays.stream((new String[]{"Owen Wilson", "Rachel McAdams", "Kathy Bates"})).toList();
+        Movie movie1 = new Movie("Midnight in Paris","2011","PG-13","10 Jun 2011",
+                "94 min","Comedy, Fantasy, Romance","Woody Allen","Woody Allen",actors);
+        List<Movie> movies = new ArrayList<>();
+        movies.add(movie1);
+
+        MovieRecord record = new MovieRecord(2, 10, 26, 3, movies);
+
+        List<String> directors = new ArrayList<>();
+        directors.add("Woody Allen");
+        directors.add("Martin Scorsese");
+        when(this.service.getDirectors(2)).thenReturn(directors);
+        MovieRecord expectation = RestClient.create().get()
+                .uri(MOVIES_URL, 2)
+                .retrieve()
+                .body(MovieRecord.class);
+        assertThat(expectation).isEqualTo(record);
+    }
 }
