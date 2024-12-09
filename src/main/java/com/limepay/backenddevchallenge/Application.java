5216c8921d76d2fde6e543d5feaf2e638fdd0dff
william-atienza <william.atienza@yahoo.com>
2024-12-09 18:22:38 +0800

    Rename Controller and Application classes

--- src/main/java/com/limepay/backenddevchallenge/Application.java
+++ src/main/java/com/limepay/backenddevchallenge/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackenddevchallengeApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackenddevchallengeApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
